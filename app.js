363419bbd40c35714bc1f4871c3c355c1fd19941
Broma Gift <elenwogift2@gmail.com>
2020-08-20 13:41:40 +0100

    my first commit

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const progressBar = document.querySelector('#progress');
+const leaderboardBtn = document.querySelector('#leaderboard');
+const hideDiv = document.querySelector('.hide');
+const dispalyDiv = document.querySelector(".leader-board");
+const backBtn = document.querySelector('#back');
+let error = document.querySelector('.error');
+
+//add votes to user score and reduce total votes
 const increaseCount = () => {
     const totalVotes = parseInt(document.querySelector("h1").innerHTML);
     const className = this.event.target.className;
@@ -5,20 +13,18 @@ const increaseCount = () => {
     spans.forEach((span) => {
       if (span.classList[0] === className) {
         const spanValue = parseInt(span.innerHTML);
-        if (totalVotes <= 150 && spanValue <= 150) {
+        if (totalVotes <= 100 && spanValue <= 100) {
           if (totalVotes === 0) {
             return;
           }
           document.querySelector("h1").innerHTML = totalVotes - 1;
           span.innerHTML = spanValue + 1;
-        }
+        }      
       }
-      const progress = document.querySelector('#progressbar');
     });
   };
-
-
  
+  //subtraact votes from user score and add to total votes
   const decreaseCount = () => {
     const totalVotes = parseInt(document.querySelector("h1").innerHTML);
    const  className = this.event.target.className;
@@ -26,8 +32,8 @@ const increaseCount = () => {
     spans.forEach((span) => {
       if (span.classList[0] === className) {
         const spanValue = parseInt(span.innerHTML);
-        if (totalVotes < 150 || spanValue < 150) {
-          if (spanValue < 1 || totalVotes === 150) {
+        if (totalVotes < 100 || spanValue < 100) {
+          if (spanValue < 1 || totalVotes === 100) {
             return;
           }
           document.querySelector("h1").innerHTML = totalVotes + 1;
@@ -36,3 +42,23 @@ const increaseCount = () => {
       }
     });
   };
+
+
+  //bring up leaderBoard at button click
+    leaderboardBtn.addEventListener('click', () => {
+      let numberVotes = parseInt(document.querySelector('.board-num').innerHTML);
+      if (numberVotes === 0) {
+        hideDiv.style.display = "none";
+        dispalyDiv.style.display = "block";
+      } else 
+      error.innerHTML = '*finish the votes';
+ })
+  
+ backBtn.addEventListener('click', () => {
+   dispalyDiv.style.display = "none";
+   hideDiv.style.display = "block";
+ })
+
+
+ //sort leaderboard 
+
